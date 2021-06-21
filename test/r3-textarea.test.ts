5417cd1201558c0c968e0c823176b1817105832b
NeoTRAN001 <neo_tran@outlook.com>
2021-06-20 19:09:13 -0500

    feat testing and ui

--- test/r3-textarea.test.ts
+++ test/r3-textarea.test.ts
@@ -1,32 +1,115 @@
-import { html, fixture, expect } from '@open-wc/testing';
-
+import { html, fixture, expect, assert } from '@open-wc/testing';
 import { R3Textarea } from '../src/R3Textarea.js';
 import '../r3-textarea.js';
 
+let el: R3Textarea;
+
+const cleanComponent = async () => {
+  el = await fixture<R3Textarea>(html`<r3-textarea></r3-textarea>`);
+  await el.updateComplete;
+}
+
+const getAttribute = (selector: String = '', attribute: string) => {
+  return el.shadowRoot?.querySelector(`${selector}`)?.getAttribute(attribute);
+}
+
 describe('R3Textarea', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture<R3Textarea>(html`<r3-textarea></r3-textarea>`);
+  describe('Default value', () => {
+    cleanComponent();
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    it('Props value', () => {
+      expect(el.ambient).to.equal('light');
+      assert.typeOf(el.ambient, 'String');
+
+      expect(el.height).to.equal(10);
+      assert.typeOf(el.height, 'Number');
+      
+      expect(el.label).to.equal('Default Text');
+      assert.typeOf(el.label, 'String');
+
+      expect(el.placeholder).to.equal('');
+      assert.typeOf(el.placeholder, 'String');
+
+      expect(el.readOnly).to.be.false;
+      assert.typeOf(el.readOnly, 'Boolean');
+
+      expect(el.required).to.be.false;
+      assert.typeOf(el.required, 'Boolean');
+
+      expect(el.value).to.equal('');
+      assert.typeOf(el.value, 'String');
+
+      expect(el.width).to.equal(20);
+      assert.typeOf(el.width, 'Number');
+
+      expect(el.withLabel).to.be.false;
+      assert.typeOf(el.withLabel, 'Boolean');
+    });
+
+    it('View ambient to container', () => {
+      const container = getAttribute('#container', 'class');
+      expect(container).to.equal('light');
+    });
+
+    it('View label not displayed', () => {
+      const labelText = el.shadowRoot?.querySelector('#label')?.textContent;
+      expect(labelText).to.equal(undefined);
+    });
+
+    it('Value in textarea is empty', () => {
+      const value = el.shadowRoot?.querySelector('#textarea')?.textContent;
+      expect(value).to.equal('');
+    });
+
+    it('Textarea size', () => {
+      const size = getAttribute('#textarea', 'style');
+      expect(size).to.equal('width: 20rem; height: 10rem;');
+    });
+
+    it('Placeholder in Textarea', () => {
+      const placeholder = getAttribute('#textarea', 'placeholder');
+      expect(placeholder).to.equal('');
+    });
   });
 
-  it('increases the counter on button click', async () => {
-    const el = await fixture<R3Textarea>(html`<r3-textarea></r3-textarea>`);
-    el.shadowRoot!.querySelector('button')!.click();
+  describe('Change value into props', () => {
+    cleanComponent();
 
-    expect(el.counter).to.equal(6);
-  });
+    it('View ambient to container', async () => {
+      el.ambient = 'dark';
+      await el.updateComplete;
+      const container = getAttribute('#container', 'class');
+      expect(container).to.equal('dark');
+    });
 
-  it('can override the title via attribute', async () => {
-    const el = await fixture<R3Textarea>(html`<r3-textarea title="attribute title"></r3-textarea>`);
+    it('View label text in Dom', async () => {
+      el.label = 'CUSTOM TEXT';
+      el.withLabel = true;
+      await el.updateComplete;
+      const labelText = el.shadowRoot?.querySelector('#label')?.textContent;
+      expect(labelText).to.equal('CUSTOM TEXT');
+    });
 
-    expect(el.title).to.equal('attribute title');
-  });
+    it('Get value in textarea from DOM', async () => {
+      el.value = 'CUSTOM TEXT';
+      await el.updateComplete;
+      const value = el.shadowRoot?.querySelector('#textarea')?.textContent;
+      expect(value).to.equal('CUSTOM TEXT');
+    });
 
-  it('passes the a11y audit', async () => {
-    const el = await fixture<R3Textarea>(html`<r3-textarea></r3-textarea>`);
+    it('Textarea size', async () => {
+      el.width = 300;
+      el.height = 200;
+      await el.updateComplete;
+      const size = getAttribute('#textarea', 'style');
+      expect(size).to.equal('width: 300rem; height: 200rem;');
+    });
 
-    await expect(el).shadowDom.to.be.accessible();
+    it('Placeholder in Textarea', async () => {
+      el.placeholder = 'CUSTOM PLACEHOLDER';
+      await el.updateComplete;
+      const placeholder = getAttribute('#textarea', 'placeholder');
+      expect(placeholder).to.equal('CUSTOM PLACEHOLDER');
+    });
   });
 });
