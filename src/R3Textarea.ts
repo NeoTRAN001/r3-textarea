1ec9c2550f2d73910b129b3a82f0594c9eaceaba
NeoTRAN001 <neo_tran@outlook.com>
2021-06-22 12:39:21 -0500

    feat: add description, change property

--- src/R3Textarea.ts
+++ src/R3Textarea.ts
@@ -1,14 +1,18 @@
+/* eslint import/extensions: "off" */
 import { html, LitElement, property } from 'lit-element';
 import style from './R3TextareaSyle';
 
 export class R3Textarea extends LitElement {
   static styles = [style];
 
-  @property({type: String})
+  @property({ type: String })
   ambient = 'light';
 
-  @property({type: Number})
-  height = 10;
+  @property({ type: String })
+  description = 'Default Description';
+
+  @property({ type: String, attribute: 'description-type' })
+  descriptionType = 'info';
 
   @property({ type: String })
   label = 'Default Text';
@@ -22,36 +26,50 @@ export class R3Textarea extends LitElement {
   @property({ type: Boolean })
   required = false;
 
+  @property({ type: Array })
+  size = { width: 20, height: 10 };
+
   @property({ type: String })
   value = '';
 
-  @property({type: Number})
-  width = 20;
+  @property({ type: Boolean, attribute: 'with-description' })
+  withDescription = false;
 
   @property({ type: Boolean, attribute: 'with-label' })
   withLabel = false;
 
+  get _getDescription() {
+    return this.withDescription
+      ? html`
+          <label id="description" class="${this.descriptionType}"
+            >${this.description}</label
+          >
+        `
+      : '';
+  }
+
   get _getLabel() {
-    return this.withLabel ? html`
-      <label id="label">${this.label}</label>
-    ` : '';
+    return this.withLabel
+      ? html` <label id="label">${this.label}</label> `
+      : '';
   }
 
   get _getTextArea() {
     return html`
-      <textarea 
+      <textarea
         id="textarea"
         placeholder="${this.placeholder}"
-        style="width: ${this.width}rem; height: ${this.height}rem;"
-      >${this.value}</textarea>   
+        style="width: ${this.size.width}rem; height: ${this.size.height}rem;"
+      >
+${this.value}</textarea
+      >
     `;
   }
 
   render() {
     return html`
       <div id="container" class="${this.ambient}">
-        ${this._getLabel}
-        ${this._getTextArea}
+        ${this._getLabel} ${this._getTextArea} ${this._getDescription}
       </div>
     `;
   }
